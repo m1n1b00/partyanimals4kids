f7c3580cd18c81fa5f2d71a7886c59a4c8f7687c
Jennifer Bakker <miniboo@gmail.com>
2011-05-12 21:16:42 -0700

    Brand new rails app for PartyAnimals4kids.com

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Partyanimals4kids::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Partyanimals4kids::Application.config.secret_token = '8a29610985fc8d799e4026c8832577676a15d64d97eda765eadfd33ca90b1a0ff44045c69c873b2a72e2c335b79dc97c01f4ee325d1da5f6b33acebc39662a8f'
